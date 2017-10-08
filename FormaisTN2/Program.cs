27d439e2b9c2df93f0d4c65c9375aa39725e1773
Luiz Alberto Laus da Rosa <luiz.alberto.laus.rosa@gmail.com>
2017-10-08 02:18:29 -0300

    gui

---
+++ FormaisTN2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace CGM1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new GUI());
+		}
+	}
+}
